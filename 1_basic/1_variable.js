cd38c219e02b71e70ba6a09fe851e50b66fcd5b0
Gitabhi07 <88783656+Gitabhi07@users.noreply.github.com>
2025-03-05 12:48:33 +0000

    datatype

---
+++ 1_basic/1_variable.js
@@ -0,0 +1,15 @@
+const accountId = 12345;
+let accountEmail = 'qwerty@gmail.com'
+var accountPassword = 'qwerty123'
+acooountaddress = 'ghazipur'
+
+
+// console.log(accountId)
+
+accountEmail= 'a@a.com'
+accountPassword= 'a123'
+acooountaddress= 'delhi'
+
+// var data type ko nahi use krna hai aur js mein scope ka concept nahi tha ab hai..
+
+console.table([accountId, accountEmail, accountPassword, acooountaddress])
---
+++ 1_basic/2_datatype.js
@@ -0,0 +1,5 @@
+age = 4
+console.log(typeof null);// object
+console.log(typeof undefined);// undefined
+console.log(typeof NaN);// number
+console.log(typeof Infinity);// number
