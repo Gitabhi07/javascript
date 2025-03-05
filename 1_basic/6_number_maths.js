4525abb6385761284cc20e37dd54a4c3b826517e
Gitabhi07 <88783656+Gitabhi07@users.noreply.github.com>
2025-03-05 21:43:00 +0000

    number&mathfun

---
+++ 1_basic/6_number_maths.js
@@ -0,0 +1,52 @@
+// ----------------- Number Methods ----------------->
+const score = 400.675
+
+const balance = new Number(100)
+// console.log(balance);
+
+
+// console.log(balance.valueOf());
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+// console.log(balance.toPrecision(2));
+// console.log(balance.toExponential(2));
+// console.log(score.toPrecision(5));
+// console.log(score.toExponential(2));
+// console.log(score.toString());
+// console.log(score.toFixed(2));
+// console.log(score.valueOf());
+
+// ------------------ Maths library ------------------>
+
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.abs(-5));
+console.log(Math.LN2);
+console.log(Math.LN10);
+console.log(Math.LOG2E);
+console.log(Math.LOG10E);
+console.log(Math.SQRT1_2);
+console.log(Math.SQRT2);
+console.log(Math.ceil(5.1));
+console.log(Math.floor(5.9));
+console.log(Math.round(5.5));
+console.log(Math.max(5, 10, 20, 30, 40, 50));
+console.log(Math.min(5, 10, 20, 30, 40, 50));
+console.log(Math.pow(2, 3));
+console.log(Math.sqrt(25));
+console.log(Math.random());
+console.log(Math.random() * 100);
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.floor(Math.random() * 100)+1);
+console.log(Math.sin(0));
+console.log(Math.cos(0));
+console.log(Math.tan(0));
+
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
