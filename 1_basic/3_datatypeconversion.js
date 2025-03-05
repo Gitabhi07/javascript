dfb7287de68784eb3fc5560e9fbe5cb45b21c73a
Gitabhi07 <88783656+Gitabhi07@users.noreply.github.com>
2025-03-05 13:07:58 +0000

    conversiondatatype

---
+++ 1_basic/3_datatypeconversion.js
@@ -0,0 +1,16 @@
+// let score = '33';
+
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// Convert the score to a string
+// let scoreString = score.toString();
+// console.log(scoreString);
+
+// check a boolean datatype
+
+let loggedIn = "edgf";
+console.log(Boolean(loggedIn));
