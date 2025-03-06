bd4595afc6c3500641f357219799016e7252ac26
Gitabhi07 <88783656+Gitabhi07@users.noreply.github.com>
2025-03-06 20:57:45 +0000

    logic_arrayfile

---
+++ 2_basic/1_array.js
@@ -0,0 +1,30 @@
+// node 2_basic/1_array.js
+
+// Array
+// An array is a special variable, which can hold more than one value at a time.
+// If you have a list of items (a list of car names, for example), storing the cars in single variables could look like this:
+
+const cars = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(cars);
+
+// --------------------------------------->>>>>>> Access the Elements of an Array
+// You refer to an array element by referring to the index number.
+// This statement accesses the value of the first element in cars:
+console.log(cars[0]);
+
+// let unishift = cars.unshift(0);
+// console.log(unishift);
+// console.log(cars);
+
+// unshift() method adds new items to the beginning of an array, and returns the new length.
+// The unshift() method adds one or more elements to the beginning of an array and returns th
+
+// let shift = cars.shift();
+// console.log(shift);
+// console.log(cars);
+
+// shift() method removes the first item from an array and returns that item.
+// The shift() method removes the first item of an array.
+
+length = cars.length;
+console.log(length);
